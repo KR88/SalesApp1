23b59b2441cc5e63ee6ccbc28e619f74611dab02
krossi <krossi@DESKTOP-HF31LJS>
2020-05-01 10:47:40 -0700

    Console write and read for UI

--- SalesApp/Program.cs
+++ SalesApp/Program.cs
@@ -8,7 +8,37 @@ namespace SalesApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome Allied Pioneer Ltd.");
+            while (true)
+                Console.WriteLine("0. Exit");
+            Console.WriteLine("1. Create an account");
+            Console.WriteLine("2. Print all accounts");
+            Console.WriteLine("3. Sell parts");
+            Console.WriteLine("4. Purchase parts");
+            Console.WriteLine("5. Print all transactions");
 
+            var option = Console.ReadLine();
+            switch(option)
+            {
+                case "0":
+                    Console.WriteLine(" Thanks for doing business with us");
+                    return;
+
+                case "1":
+                    Console.WriteLine(" Account name ");
+                    var accountName = Console.ReadLine();
+                    Console.WriteLine(" Email address ");
+                    var emailAddress = Console.ReadLine();
+                    var accountTypes = Enum.GetNames(typeof(TypeOfAccount));
+                    for (var i = 0; i < accountTypes.Length; i++)
+                    {
+                        Console.WriteLine($"{i} . {accountTypes[i]}");
+                        var accountType = Enum.Parse<TypeOfAccount>(Console.ReadLine());
+
+                    }
+
+
+            }
 
         }
         
